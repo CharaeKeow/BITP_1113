1ba12202a886a67c1907e80778cc87300808d6b7
CharaeKeow <34004186+CharaeKeow@users.noreply.github.com>
2019-09-25 00:42:47 +0800

    basic CPP program

---
+++ Exercise 1 Q1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Universiti: Universiti Teknikal Malaysia, Melaka" << endl;
+	cout << "Faculty: FTMK" << endl;
+	cout << "Programme: BITI" << endl;
+	cout << "\n\n" << "My name is Charae a/L Eh Sin. I am from Kelantan." << endl;
+
+	return 0;
+}
